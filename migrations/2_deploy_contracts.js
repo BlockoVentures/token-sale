8e62e7a565e653d282551bd99bb64bf34252ca66
Alex Figueiredo <alexxfsilva@gmail.com>
2017-10-26 16:09:51 -0200

    Added dates to deploy on testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,10 @@ const BLOCCrowdsale = artifacts.require("./BLOCCrowdsale.sol");
 module.exports = function(deployer, network, accounts) {
 
     const wallet       = accounts[0];
-    const presaleStart = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1;
-    const presaleEnd   = presaleStart + (86400 * 30); // 30 days
-    const saleEnd      = presaleEnd + (86400 * 30); // 30 days
+    const presaleStart = 1509041700; // Thursday, October 26, 2017 6:15:00 PM
+    const presaleEnd   = 1509042000; // Thursday, October 26, 2017 6:20:00 PM
+    const saleEnd      = 1509128400; // Thursday, October 27, 2017 6:20:00 PM
 
-    deployer.deploy(BLOCPresale, presaleStart, presaleEnd, wallet);
-    deployer.deploy(BLOCCrowdsale, presaleStart, saleEnd, wallet);
+    deployer.deploy(BLOCPresale, presaleStart, presaleEnd, wallet, { gas: 7000000 });
+    deployer.deploy(BLOCCrowdsale, presaleEnd, saleEnd, wallet, { gas: 7000000 });
 };
