5c307478caa66219eb89634d554b289b18f5d572
Alex Figueiredo <alexxfsilva@gmail.com>
2017-10-26 16:20:07 -0200

    Modified date and account

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,10 @@ const BLOCCrowdsale = artifacts.require("./BLOCCrowdsale.sol");
 module.exports = function(deployer, network, accounts) {
 
     const wallet       = accounts[0];
-    const presaleStart = 1509041700; // Thursday, October 26, 2017 6:15:00 PM
-    const presaleEnd   = 1509042000; // Thursday, October 26, 2017 6:20:00 PM
+    const presaleStart = 1509042600; // Thursday, October 26, 2017 6:30:00 PM
+    const presaleEnd   = 1509043200; // Thursday, October 26, 2017 6:40:00 PM
     const saleEnd      = 1509128400; // Thursday, October 27, 2017 6:20:00 PM
 
-    deployer.deploy(BLOCPresale, presaleStart, presaleEnd, wallet, { from: accounts[1], gas: 7000000 });
-    deployer.deploy(BLOCCrowdsale, presaleEnd, saleEnd, wallet, { from: accounts[1], gas: 7000000 });
+    deployer.deploy(BLOCPresale, presaleStart, presaleEnd, wallet, { gas: 7000000 });
+    deployer.deploy(BLOCCrowdsale, presaleEnd, saleEnd, wallet, { gas: 7000000 });
 };
